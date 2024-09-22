95ee9eab468e3018f352a86879d30590a41571ba
kongnyuy lola <kongnyuybenardlola@gmail.com>
2024-09-22 11:46:24 +0100

    setup our app to use routeProvider with createBrowersRouter for router and update our menu-item to navigate with useNavigate

--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const HatsPage = () => {
+  return (<div>
+    <h1>Hats Page</h1>
+  </div>)
+}
+ 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    errorElement: <div>404 NOT FOUND</div>
+  },
+  {
+    path: "/shop/hats",
+    element: <HatsPage />,
+  }
+])
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
