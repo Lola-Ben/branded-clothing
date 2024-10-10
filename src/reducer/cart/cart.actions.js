56a08a3000b602cf80a3d4b8b4d3f2ab51384db6
kongnyuy lola <kongnyuybenardlola@gmail.com>
2024-10-10 11:10:49 +0100

    Upadated cart utils to group item in cart reducer

--- src/reducer/cart/cart.actions.js
+++ src/reducer/cart/cart.actions.js
@@ -4,5 +4,8 @@ const cartAction = () => ({
     type: cartActiontypes.TOGGLE_CART_HIDDEN
 })
 
-
+export const addItem = (user) => ({
+    type: cartActiontypes.ADD_ITEM,
+    payload: user
+})
 export default cartAction;
